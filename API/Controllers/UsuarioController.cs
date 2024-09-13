34a9108b094a9aeeb2e5864f7eadda2c190c1b67
luckfer7 <lucasferreira059@gmail.com>
2024-09-13 15:39:19 -0300

    endpoint com parametro

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -21,5 +21,26 @@ namespace API.Controllers
 
             return Ok(obj);
         }
+
+        [HttpGet("Apresentar/{nome}")]    
+        public IActionResult Apresentar (string nome)
+        {
+            var mensagem = $"Olá, {nome}, seja bem vindo!";
+            return Ok(new{mensagem});
+        }
+
+        [HttpGet ("DizerIdade/{idade}")]
+        public IActionResult DizerIdade (int idade)
+        {
+            var msg = $"{idade} é a sua idade";
+            return Ok(new {msg});
+        }
+
+        [HttpGet ("InfoOfAPerson/{name}/{age}")]
+        public IActionResult InfoOfAPerson (string name, int age)
+        {
+            var message = $"Hello, {name}. You are {age} years old";
+            return Ok(new {message});
+        }
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fc876a94acb8863e64763ec59137e7be0d020f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
